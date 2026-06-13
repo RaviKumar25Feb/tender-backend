c9ee6cb00eb3a82366152a2113e85b9ccbe33ac8
ravikumar25feb <RaviKumar25Feb@users.noreply.github.com>
2026-06-13 13:28:36 +0530

    change cron time

--- cron/tenderCron.js
+++ cron/tenderCron.js
@@ -6,7 +6,7 @@ const startTenderCron = () => {
 
   console.log("🔥 Cron initialized");
 
-  cron.schedule("*/5 * * * *", async () => {
+  cron.schedule("*/2 * * * *", async () => {
     if (isRunning) return;
 
     isRunning = true;
